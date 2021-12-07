174cf5714e819e8bf6d285ec46345e073712c8a0
cristipiticul <cristipiticul@yahoo.com>
2021-12-07 02:09:31 +0200

    Added similar inputs tutorial
    Added similar inputs tutorial

--- README.md
+++ README.md
@@ -19,4 +19,6 @@ This readme is intended to help navigate the tutorial that is organized in a mod
 
 + The method after that aims at hiding pieces of the input and looking at the changes that it implies in the output. Combined with deconvolutions (the reverse operation of the convolution), it is a cool method for looking at a popular image analysis model : the CNN. The notebook for these methods can be found [here](./03_occlusion_deconv.ipynb)
 
++ Another simple method is [finding similar inputs](./06_similar_inputs.ipynb). If the network makes a mistake on a test input, we can see which training inputs are similar from network's point of view. The results can show us what features the network does not use in classification.  
+
 + Then, we will dig deeper into more sophisticated methods for explaining deep learning models, [shapley values](./04_shapley_values.ipynb) and [CAM](05_scoreCAM.ipynb).
--- data_and_models.py
+++ data_and_models.py
@@ -63,4 +63,27 @@ class mnist_binary_cnn(nn.Module):
 
         return x
 
-        
+
+# For full MNIST dataset (10 classes)
+def get_mnist_dataset(root,train):
+    return torchvision.datasets.MNIST(root=root, train=train, download=True,
+                                 transform=torchvision.transforms.ToTensor())
+
+class mnist_conv_model(nn.Module):
+    def __init__(self, loss=nn.CrossEntropyLoss()):
+        super(mnist_conv_model, self).__init__()
+        self.conv1 = nn.Conv2d(1, 5, kernel_size=5)
+        self.conv2 = nn.Conv2d(5, 2, kernel_size=5)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(800, 50)
+        self.fc2 = nn.Linear(50, 10)
+        self.softmax = nn.Softmax(dim=1)
+        self.loss=loss
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return self.softmax(x)
